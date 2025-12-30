4958cc561f5fa5bb01081c19c958a081d9a33be5
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-12-30 20:15:40 +0530

    jenkins

---
+++ 70-acm/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+variable "zone_id" {
+    default = "Z0610433P618A3VPI0AH"
+}
+
+variable "domain_name" {
+    default = "daws86pavan.cyou"
+}
+ 
---
+++ 90-eks/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0610433P618A3VPI0AH"
+}
+
+variable "domain_name" {
+    default = "daws86pavan.cyou"
+}
+ 
+ variable "eks_version" {  
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
